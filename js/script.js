11db38fc219ece206b030a06e4e81bc7a57b5ea8
Agata Siemieniec <agata.siemieniec@hotmail.com>
2020-10-09 22:23:03 +0200

    Add class to the clicked link

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const titleClickHandler = function(event){
     console.log('Link was clicked!');
     console.log(event);
   
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -10,15 +10,21 @@ const titleClickHandler = function(event){
     activeLink.classList.remove('active');
     }
   
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+
+    const clickedElement = this;
+    console.log('clickedElement:', clickedElement);
+    clickedElement.classList.add('active');  //dlaczego przed add ma być kropka ? 
+    
+
+  
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('active');
 
     for(let activeArticle of activeArticles){
     activeLink.classList.remove('.posts a.active');
     }
-  
-    /* remove class 'active' from all articles */
   
     /* get 'href' attribute from the clicked link */
   

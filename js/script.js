22a5c0696807af6d6acdb672003d5ffa443dedad
Agata Siemieniec <agata.siemieniec@hotmail.com>
2020-10-11 23:46:00 +0200

    update code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
@@ -8,16 +9,18 @@ const titleClickHandler = function(event){
   
     /* [DONE] remove class 'active' from all article links  */ 
     const activeLinks = document.querySelectorAll('.titles a.active');
+    
     for(let activeLink of activeLinks){
-        activeLink.classList.remove('active');
+      activeLink.classList.remove('active');
     }
 
     /* [DONE] add class 'active' to the clicked link */
     console.log('clickedElement:', clickedElement);
-    clickedElement.classList.add('active');  //dlaczego przed add ma być kropka ? 
+    clickedElement.classList.add('active');  
 
     /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
+
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }
